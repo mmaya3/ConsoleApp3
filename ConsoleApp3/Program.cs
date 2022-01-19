44e9af477184bb9d83ffc305b8561dc060589e52
miquel.maya <miquel.maya@institutmontilivi.cat>
2022-01-19 19:36:01 +0100

    Feina acabada

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp3
         {
             Console.WriteLine("Hello Montilivi!");
             Console.WriteLine("Estic aprenent a utilitzar GitHub");
+            Console.WriteLine("Sóc el teu company, en Miquel")
         }
     }
 }
